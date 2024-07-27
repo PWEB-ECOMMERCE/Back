1dea144030369a1a2d5261590a2c478085fcc92a
Filipe <filipecavalcantiximenes@gmail.com>
2024-07-27 16:48:38 -0300

    repositories

---
+++ src/main/java/com/example/ecommerce/repository/VendaProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.domain.venda.Venda;
+import com.example.ecommerce.domain.venda_produto.Venda_produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VendaProdutoRepository extends JpaRepository<Venda_produto, Integer> {
+}
