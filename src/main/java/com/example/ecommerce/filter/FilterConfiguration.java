0dcf16e6f219a77c1d6275221ed37b3476a312dc
Filipe <filipecavalcantiximenes@gmail.com>
2024-08-09 14:03:07 -0300

    nova rota do filtro

--- src/main/java/com/example/ecommerce/filter/FilterConfiguration.java
+++ src/main/java/com/example/ecommerce/filter/FilterConfiguration.java
@@ -17,7 +17,7 @@ public class FilterConfiguration {
 
         register.setFilter(new Filtro());
         //isso configura o filtro para que atue sobre a url abaixo, ou seja, /usuarios e todas as variações
-        register.addUrlPatterns("/usuarios/perfil/*");
+        register.addUrlPatterns("/usuarios/esp/*");
         return register;
     }
 }
