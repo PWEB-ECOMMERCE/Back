698ed46b5c4fcf3e7413962f3b8034ff3c55943c
Filipe <filipecavalcantiximenes@gmail.com>
2024-08-10 15:11:38 -0300

    new session register

--- src/main/java/com/example/ecommerce/controllers/UsuarioController.java
+++ src/main/java/com/example/ecommerce/controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import com.example.ecommerce.dto.usuario.UsuarioRequestDTO;
 import com.example.ecommerce.dto.usuario.UsuarioResponseDTO;
 import com.example.ecommerce.services.UsuarioService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -56,10 +57,11 @@ public class UsuarioController {
         return ResponseEntity.ok().body(dadosUsuario);
     }
     @PostMapping
-    public ResponseEntity<UsuarioIdDTO> registerUsuario(@RequestBody UsuarioRequestDTO newUserData, UriComponentsBuilder uriComponentsBuilder){
+    public ResponseEntity<UsuarioIdDTO> registerUsuario(@RequestBody UsuarioRequestDTO newUserData, UriComponentsBuilder uriComponentsBuilder, HttpServletRequest request, HttpServletResponse response){
         UsuarioIdDTO usuarioRegistradoID = this.usuarioService.registerUsuario(newUserData);
-
-        URI usuarioURI = uriComponentsBuilder.path("/usuarios/{usuarioID}").buildAndExpand(usuarioRegistradoID.usuarioUUID()).toUri();
+        HttpSession session = request.getSession();
+        session.setAttribute("usuario", usuarioRegistradoID);
+        URI usuarioURI = uriComponentsBuilder.path("/usuarios/esp/{usuarioID}").buildAndExpand(usuarioRegistradoID.usuarioUUID()).toUri();
 
         return ResponseEntity.created(usuarioURI).body(usuarioRegistradoID);
     }
