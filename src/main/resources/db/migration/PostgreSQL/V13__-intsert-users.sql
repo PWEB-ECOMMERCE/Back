71b0b511dce803d28bd9093096a38ffee2581c53
Filipe <filipecavalcantiximenes@gmail.com>
2024-09-13 07:32:59 -0300

    insercao bd

---
+++ src/main/resources/db/migration/PostgreSQL/V13__-intsert-users.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuario VALUES ('b2fd337e-726f-4028-8697-01d8b837a324', 'dante araujo', 'dantearaujo', '$2a$10$9Fm7ulkgUDzvOc1Abgj4DOoU1SV9ZxmwU0SiLegxZDl5J7oJGOoga', 'rua antonele', 'danteeng@hotmail.com', false);
+INSERT INTO usuario VALUES ('48116543-df63-43de-8ce6-6ea71e97c4e0', 'Leo Professor', 'leoprofessor', '$2a$10$kofjvUhE3EX4gTrz8DdYROwVkv0H5lArvOzBmvF5mMQHQTN/F4B02', 'SMD', 'leo@domain.com', false);
