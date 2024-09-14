a80462fb0625110437cd9eb855dfa669d0241256
Filipe <filipecavalcantiximenes@gmail.com>
2024-09-14 13:30:49 -0300

    relatorio de compras com dto

--- src/main/java/com/example/ecommerce/controllers/RelatorioController.java
+++ src/main/java/com/example/ecommerce/controllers/RelatorioController.java
@@ -1,17 +1,21 @@
 package com.example.ecommerce.controllers;
 
+import com.example.ecommerce.domain.usuario.Usuario;
 import com.example.ecommerce.domain.venda.ComprasInterface;
 import com.example.ecommerce.domain.venda.Venda;
 import com.example.ecommerce.dto.produto.ProdutoResponseDTO;
 import com.example.ecommerce.dto.relatorio.ComprasClientes;
+import com.example.ecommerce.dto.relatorio.RelatorioComprasResponseDTO;
 import com.example.ecommerce.repository.ProdutoRepository;
 import com.example.ecommerce.repository.VendaRepository;
+import com.example.ecommerce.services.UsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,13 +24,26 @@ import java.util.List;
 public class RelatorioController {
     private final VendaRepository vendaRepository;
     private final ProdutoRepository produtoRepository;
+    private final UsuarioService usuarioService;
 
     @GetMapping("/compras")
-    public ResponseEntity<List<ComprasInterface>> comprasClientes(){
-        //List<Venda> vendas = vendaRepository.finda().stream().toList();
-        List<ComprasInterface> vendas = vendaRepository.finda().stream().toList();
+    public ResponseEntity<List<RelatorioComprasResponseDTO>> comprasClientes(){
 
-        return ResponseEntity.ok().body(vendas);
+        List<ComprasInterface> vendas = vendaRepository.finda().stream().toList();
+        List<RelatorioComprasResponseDTO> relatorioComprasResponseDTOS = new ArrayList<>();
+
+        for(int i = 0; i < vendas.size(); i++) {
+            Usuario usuario = usuarioService.getUsuarioByID(vendas.get(i).getUsuario_id());
+            RelatorioComprasResponseDTO relatorioComprasResponseDTO = new RelatorioComprasResponseDTO(
+                    usuario.getId(),
+                    usuario.getNome(),
+                    Integer.parseInt(vendas.get(i).getNum()));
+            relatorioComprasResponseDTOS.add(relatorioComprasResponseDTO);
+        }
+
+
+
+        return ResponseEntity.ok().body(relatorioComprasResponseDTOS);
     }
 
 
--- src/main/java/com/example/ecommerce/domain/venda/ComprasInterface.java
+++ src/main/java/com/example/ecommerce/domain/venda/ComprasInterface.java
@@ -2,5 +2,6 @@ package com.example.ecommerce.domain.venda;
 
 public interface ComprasInterface {
     String getUsuario_id();
+    String getNum();
 
 }
