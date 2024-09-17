c7bab13a27440cf0114ec4a17a7c1ac504e1d2d3
Filipe <filipecavalcantiximenes@gmail.com>
2024-09-17 13:48:21 -0300

    dados bd

---
+++ src/main/resources/db/migration/PostgreSQL/V12__insert-values-db.sql
@@ -0,0 +1,6 @@
+INSERT INTO usuario VALUES ('b2fd337e-726f-4028-8697-01d8b837a324', 'dante araujo', 'dantearaujo', '$2a$10$9Fm7ulkgUDzvOc1Abgj4DOoU1SV9ZxmwU0SiLegxZDl5J7oJGOoga', 'rua antonele', 'danteeng@hotmail.com', false);
+INSERT INTO usuario VALUES ('48116543-df63-43de-8ce6-6ea71e97c4e0', 'Leo Professor', 'leoprofessor', '$2a$10$kofjvUhE3EX4gTrz8DdYROwVkv0H5lArvOzBmvF5mMQHQTN/F4B02', 'SMD', 'leo@domain.com', false);
+INSERT INTO venda VALUES(1, '2024-09-11 10:42:22.400736', 'b2fd337e-726f-4028-8697-01d8b837a324');
+INSERT INTO venda VALUES(2, '2024-09-13 07:20:12.324129', 'b2fd337e-726f-4028-8697-01d8b837a324');
+INSERT INTO venda_produto VALUES(1, 4, 2);
+INSERT INTO venda_produto VALUES(2, 3, 4);
