3018d7907b7902bb8cf673f9fbe4fd94ce865efa
Filipe <filipecavalcantiximenes@gmail.com>
2024-09-19 14:13:08 -0300

    limpadinha

--- src/main/java/com/example/ecommerce/controllers/RelatorioController.java
+++ src/main/java/com/example/ecommerce/controllers/RelatorioController.java
@@ -1,11 +1,8 @@
 package com.example.ecommerce.controllers;
 
-import com.example.ecommerce.domain.produto.Produto;
 import com.example.ecommerce.domain.usuario.Usuario;
 import com.example.ecommerce.domain.venda.ComprasInterface;
-import com.example.ecommerce.domain.venda.Venda;
 import com.example.ecommerce.domain.venda.VendasDiaInterface;
-import com.example.ecommerce.domain.venda_produto.VendaProduto;
 import com.example.ecommerce.dto.produto.ProdutoResponseDTO;
 import com.example.ecommerce.dto.relatorio.RelatorioComprasResponseDTO;
 import com.example.ecommerce.dto.relatorio.VendasPorTempoDTO;
@@ -21,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @RequiredArgsConstructor
 @RestController
@@ -65,13 +61,6 @@ public class RelatorioController {
     public ResponseEntity<List<VendasPorTempoDTO>> totalValorPorDia(@RequestParam("data") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate data,
                                                                     @RequestParam("endData") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endData) {
 
-
-        List<VendaProduto> vendaProdutoList = vendaProdutoRepository.findAll();
-
-        List<AtomicInteger> valoresTotals = new ArrayList<>();
-        AtomicInteger valorTotal = new AtomicInteger();
-
-        List<String> vendas = vendaRepository.findByData_hora(data);
         List<VendasDiaInterface> vendasDia = vendaRepository.findVendasPeriodoTempo(data, endData);
         List<VendasPorTempoDTO> vendasPorTempoDTOList = new ArrayList<>();
 
@@ -79,8 +68,6 @@ public class RelatorioController {
             VendasPorTempoDTO vendasPorTempoDTO = new VendasPorTempoDTO(venda.getData_hora(), venda.getValor_total());
             vendasPorTempoDTOList.add(vendasPorTempoDTO);
         });
-
-
         return ResponseEntity.ok().body(vendasPorTempoDTOList);
     }
 
